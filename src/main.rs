123e9c1a560bb3d39b4128f2e54182104a7a8b19
Jeff Hage <jeffreyhage@gmail.com>
2023-05-25 12:27:12 -0400

    my first test

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,18 @@ struct AppState {
 async fn main() {
     tracing_subscriber::fmt::init();
 
+    let addr = &"0.0.0.0:3000".parse().unwrap();
+
+    tracing::info!("listening on {}", addr);
+
+    // Run with hyper
+    axum::Server::bind(&addr)
+        .serve(app().into_make_service())
+        .await
+        .unwrap();
+}
+
+fn app() -> Router {
     // Create dir
     fs::create_dir_all(StdPath::new("target").join("heed.mdb")).unwrap();
 
@@ -32,22 +44,11 @@ async fn main() {
     // Create shared state to pass around the db ref
     let shared_state = Arc::new(AppState { kv_env: env, kv });
 
-    // Build our application with a route
-    let app = Router::new()
+    Router::<Arc<AppState>>::new()
         .route("/", get(get_all))
         .route("/:key", get(get_key))
         .route("/", post(create_key))
-        .with_state(shared_state);
-
-    let addr = &"0.0.0.0:3000".parse().unwrap();
-
-    tracing::info!("listening on {}", addr);
-
-    // Run with hyper
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+        .with_state(shared_state)
 }
 
 async fn get_all(State(state): State<Arc<AppState>>) -> impl IntoResponse {
@@ -67,6 +68,12 @@ async fn get_key(State(state): State<Arc<AppState>>, Path(key): Path<String>) ->
     (StatusCode::OK, Json(KVPayload { key, value }))
 }
 
+#[derive(Serialize, Deserialize)]
+struct KVPayload {
+    key: String,
+    value: String,
+}
+
 async fn create_key(
     State(state): State<Arc<AppState>>,
     Json(payload): Json<KVPayload>,
@@ -83,8 +90,26 @@ async fn create_key(
     StatusCode::CREATED
 }
 
-#[derive(Serialize, Deserialize)]
-struct KVPayload {
-    key: String,
-    value: String,
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::{
+        body::Body,
+        http::{self, Request, StatusCode},
+    };
+    use serde_json::{json, Value};
+    use std::net::{SocketAddr, TcpListener};
+    use tower::Service; // for `call`
+    use tower::ServiceExt; // for `oneshot` and `ready`
+
+    // You can use `ready()` and `call()` to avoid using `clone()`
+    // in multiple request
+    #[tokio::test]
+    async fn get_request() {
+        let mut app = app();
+
+        let request = Request::builder().uri("/").body(Body::empty()).unwrap();
+        let response = app.ready().await.unwrap().call(request).await.unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+    }
 }
