7b9d6934eb4d2623069c6f666ee82f207c4d3092
Jeff Hage <jeffreyhage@gmail.com>
2023-05-26 11:12:06 -0400

    tests, readme

---
+++ readme.md
@@ -0,0 +1,31 @@
+# kv
+
+## What
+- This is a key-value store exposed via an Axum HTTP API and persisted on disk via [meilisearch/heed](https://github.com/meilisearch/heed).
+
+## Why
+- I wanted to learn more about Rust and Axum, so I decided to build a simple key-value store.
+
+## Usage
+- You can use it by running `cargo run` in the root directory of the project. This will start the server at `localhost:3000`.
+
+## Configuration
+- You can configure the server by setting the following environment variables:
+    - `DB_PATH`: The directory to store the data in. Defaults to `./db/heed.mdb`.
+
+## Backup / Restore
+- You can backup the data by copying the `DB_PATH` directory.
+- You can restore the data by replacing the `DB_PATH` directory with the backup.
+- This can easily be stored in S3/R2 blob storage.
+
+# TODO
+- [x] Add tests
+- [ ] Conform to proper REST API standards
+- [ ] Add more documentation
+- [ ] Add more configuration options
+- [ ] Add more error handling, I think we should be able to use the `?` syntax more
+- [ ] Add more logging
+- [ ] Add more metrics
+- [ ] Add more security
+- [ ] Add more validation
+
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use axum::extract::Path;
-use axum::routing::get;
+use axum::routing::{delete, get};
 use axum::{extract::State, http::StatusCode, response::IntoResponse, routing::post, Json, Router};
 use heed::{types::Str, Env};
 use heed::{Database, EnvOpenOptions};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::fs;
-use std::path::Path as StdPath;
 use std::sync::Arc;
 
 struct AppState {
@@ -30,13 +29,13 @@ async fn main() {
 }
 
 fn app() -> Router {
+    let db_path = std::env::var("DB_PATH").unwrap_or_else(|_| String::from("db/heed.mdb"));
+
     // Create dir
-    fs::create_dir_all(StdPath::new("target").join("heed.mdb")).unwrap();
+    fs::create_dir_all(&db_path).unwrap();
 
     // Create env
-    let env = EnvOpenOptions::new()
-        .open(StdPath::new("target").join("heed.mdb"))
-        .unwrap();
+    let env = EnvOpenOptions::new().open(&db_path).unwrap();
 
     // We will open the default unamed database
     let kv: Database<Str, Str> = env.create_database(None).unwrap();
@@ -45,9 +44,17 @@ fn app() -> Router {
     let shared_state = Arc::new(AppState { kv_env: env, kv });
 
     Router::<Arc<AppState>>::new()
+        // GET /
         .route("/", get(get_all))
+        // GET /:key
         .route("/:key", get(get_key))
+        // POST /
         .route("/", post(create_key))
+        // DELETE /
+        .route("/", delete(delete_all))
+        // DELETE /:key
+        .route("/:key", delete(delete_key))
+        // Add shared state
         .with_state(shared_state)
 }
 
@@ -63,9 +70,19 @@ async fn get_all(State(state): State<Arc<AppState>>) -> impl IntoResponse {
 async fn get_key(State(state): State<Arc<AppState>>, Path(key): Path<String>) -> impl IntoResponse {
     let rtxn = state.kv_env.read_txn().unwrap();
 
-    let value = state.kv.get(&rtxn, &key).unwrap().unwrap().to_string();
+    let value = state.kv.get(&rtxn, &key);
 
-    (StatusCode::OK, Json(KVPayload { key, value }))
+    match value {
+        Ok(Some(value)) => (StatusCode::OK, Json(json!({ "key": key, "value": value }))),
+        Ok(None) => (
+            StatusCode::NOT_FOUND,
+            Json(json!({ "error": "Key not found" })),
+        ),
+        Err(_) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(json!({ "error": "Internal server error" })),
+        ),
+    }
 }
 
 #[derive(Serialize, Deserialize)]
@@ -90,6 +107,41 @@ async fn create_key(
     StatusCode::CREATED
 }
 
+async fn delete_all(State(state): State<Arc<AppState>>) -> impl IntoResponse {
+    let mut wtxn = state.kv_env.write_txn().unwrap();
+
+    state.kv.clear(&mut wtxn).unwrap();
+
+    wtxn.commit().unwrap();
+
+    StatusCode::OK
+}
+
+async fn delete_key(
+    State(state): State<Arc<AppState>>,
+    Path(key): Path<String>,
+) -> impl IntoResponse {
+    let mut wtxn = state.kv_env.write_txn().unwrap();
+
+    let value = state.kv.delete(&mut wtxn, &key);
+
+    match value {
+        Ok(true) => {
+            wtxn.commit().unwrap();
+
+            (StatusCode::OK, Json(json!({ "key": key })))
+        }
+        Ok(false) => (
+            StatusCode::NOT_FOUND,
+            Json(json!({ "error": "Key not found" })),
+        ),
+        Err(_) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(json!({ "error": "Internal server error" })),
+        ),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -98,18 +150,154 @@ mod tests {
         http::{self, Request, StatusCode},
     };
     use serde_json::{json, Value};
-    use std::net::{SocketAddr, TcpListener};
     use tower::Service; // for `call`
     use tower::ServiceExt; // for `oneshot` and `ready`
+
+    async fn setup_tests() -> Router {
+        // set env var to use a different db
+        std::env::set_var("DB_PATH", "db/heed_test.mdb");
+
+        let mut app = app();
+
+        // Ensure db is cleared
+        let request = Request::builder()
+            .uri("/")
+            .method(http::Method::DELETE)
+            .body(Body::empty())
+            .unwrap();
+
+        app.ready().await.unwrap().call(request).await.unwrap();
+
+        app
+    }
 
     // You can use `ready()` and `call()` to avoid using `clone()`
     // in multiple request
     #[tokio::test]
-    async fn get_request() {
-        let mut app = app();
+    async fn get_all() {
+        let mut app = setup_tests().await;
 
         let request = Request::builder().uri("/").body(Body::empty()).unwrap();
         let response = app.ready().await.unwrap().call(request).await.unwrap();
+
         assert_eq!(response.status(), StatusCode::OK);
+    }
+
+    #[tokio::test]
+    async fn get_missing_key() {
+        let mut app = setup_tests().await;
+
+        let request = Request::builder()
+            .uri("/this-shouldn't-be-found")
+            .body(Body::empty())
+            .unwrap();
+        let response = app.ready().await.unwrap().call(request).await.unwrap();
+
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+    }
+
+    #[tokio::test]
+    async fn create_and_get_existing_key() {
+        let mut app = setup_tests().await;
+
+        let insert_body = json!({"key": "foo", "value": "bar"});
+
+        // Create key
+        let request = Request::builder()
+            .method(http::Method::POST)
+            .uri("/")
+            .header(http::header::CONTENT_TYPE, "application/json")
+            .body(Body::from(insert_body.to_string()))
+            .unwrap();
+
+        app.ready().await.unwrap().call(request).await.unwrap();
+
+        // Get key
+        let request = Request::builder().uri("/foo").body(Body::empty()).unwrap();
+        let response = app.ready().await.unwrap().call(request).await.unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+
+        assert_eq!(body, insert_body)
+    }
+
+    #[tokio::test]
+    async fn create_key() {
+        let mut app = setup_tests().await;
+
+        let request = Request::builder()
+            .method(http::Method::POST)
+            .uri("/")
+            .header(http::header::CONTENT_TYPE, "application/json")
+            .body(Body::from(
+                json!({"key": "foo", "value": "bar"}).to_string(),
+            ))
+            .unwrap();
+
+        let response = app.ready().await.unwrap().call(request).await.unwrap();
+
+        assert_eq!(response.status(), StatusCode::CREATED);
+    }
+
+    #[tokio::test]
+    async fn delete_all() {
+        let mut app = app();
+
+        let request = Request::builder()
+            .method(http::Method::DELETE)
+            .uri("/")
+            .body(Body::empty())
+            .unwrap();
+
+        let response = app.ready().await.unwrap().call(request).await.unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let request = Request::builder().uri("/").body(Body::empty()).unwrap();
+        let response = app.ready().await.unwrap().call(request).await.unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+
+        assert_eq!(body, json!([]));
+    }
+
+    #[tokio::test]
+    async fn delete_key() {
+        let mut app = setup_tests().await;
+
+        // Create key
+        let request = Request::builder()
+            .method(http::Method::POST)
+            .uri("/")
+            .header(http::header::CONTENT_TYPE, "application/json")
+            .body(Body::from(
+                json!({"key": "foo", "value": "bar"}).to_string(),
+            ))
+            .unwrap();
+
+        app.ready().await.unwrap().call(request).await.unwrap();
+
+        // Delete key
+        let request = Request::builder()
+            .method(http::Method::DELETE)
+            .uri("/foo")
+            .body(Body::empty())
+            .unwrap();
+
+        let response = app.ready().await.unwrap().call(request).await.unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        // Get key
+        let request = Request::builder().uri("/foo").body(Body::empty()).unwrap();
+        let response = app.ready().await.unwrap().call(request).await.unwrap();
+
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
     }
 }
